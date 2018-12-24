1e5d2bf7a25e093dc3abcacf0da79c4a473cd51e
Tyill <al_medved@mail.ru>
2018-12-24 15:19:49 +0500

    ...

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install libopenblas-base
-sudo apt-get update
+sudo apt-get update -qq;
+sudo apt-get install libopenblas-base -y;
+sudo apt-get install libopenblas-dev -y;
