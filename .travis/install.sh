514312fd9e7b48fdffd828131a3a7390a9a34f69
Tyill <al_medved@mail.ru>
2018-12-24 15:49:46 +0500

    ...

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update -qq;
-sudo apt-get install libopenblas-base -y;
-sudo apt-get install libopenblas-dev -y;
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  brew update -qq;         
+  brew install libopenblas-base -y;
+  brew install libopenblas-dev -y;  
+fi
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  sudo apt-get update -qq;
+  sudo apt-get install libopenblas-base -y;
+  sudo apt-get install libopenblas-dev -y;
+fi
