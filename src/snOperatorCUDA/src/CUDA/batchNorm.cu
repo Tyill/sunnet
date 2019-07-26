af6a89d9572b383756f7aa0eb29fc9aac35c665c
Tyill <al_medved@mail.ru>
2019-07-26 14:04:52 +0500

    -develop

--- src/snOperatorCUDA/src/CUDA/batchNorm.cu
+++ src/snOperatorCUDA/src/CUDA/batchNorm.cu
@@ -47,6 +47,8 @@ __global__ void layerBatchNormInf(snSize insz, snFloat* in, snFloat* out, batchN
     snFloat* cin = in + inStepByN * blockIdx.y + inStepByD * blockIdx.x,
            * cout = out + inStepByN * blockIdx.y + inStepByD * blockIdx.x;
     
+    bnOffset(&prm, inStepByD * blockIdx.x);
+
     unsigned int i = threadIdx.x;
     while (i < inStepByD){
 
@@ -56,96 +58,170 @@ __global__ void layerBatchNormInf(snSize insz, snFloat* in, snFloat* out, batchN
     }
 }
 
-//
-//void batchNormForward(const SN_Base::snSize& insz, snFloat* in, snFloat* out, batchNorm prm){
-//
-//    size_t inSz = insz.w * insz.h * insz.d, bsz = insz.n;
-//
-//    /// μ = 1/n * ∑x
-//    cblas_sgemv(CBLAS_ORDER::CblasRowMajor,
-//        CBLAS_TRANSPOSE::CblasTrans,
-//        blasint(bsz),
-//        blasint(inSz),
-//        1.F / bsz,
-//        in,
-//        blasint(inSz),
-//        prm.onc,
-//        blasint(1),
-//        0.F,
-//        prm.mean,
-//        blasint(1));
-//
-//    /// varce = sqrt(∑xx - mean^2 + e)
-//    for (size_t i = 0; i < inSz; ++i){
-//
-//        snFloat* cin = in + i, srq = 0.F;
-//        for (size_t j = 0; j < bsz; ++j){
-//            srq += cin[0] * cin[0];
-//            cin += inSz;
-//        }
-//        prm.varce[i] = sqrt(srq / bsz - prm.mean[i] * prm.mean[i] + 0.00001F);
-//    }
-//
-//    /// norm = (in - mean) / varce
-//    /// y = ^x * γ + β
-//    for (size_t j = 0; j < bsz; ++j){
-//
-//        snFloat* cin = in + j * inSz, *cout = out + j * inSz, *norm = prm.norm + j * inSz;
-//
-//        for (size_t i = 0; i < inSz; ++i){
-//            norm[i] = (cin[i] - prm.mean[i]) / prm.varce[i];
-//            cout[i] = norm[i] * prm.scale[i] + prm.schift[i];
-//        }
-//    }
-//
-//}
-//
-//void batchNormBackward(const SN_Base::snSize& insz, snFloat* gradIn, snFloat* gradOut, batchNorm prm){
-//    // https://kevinzakka.github.io/2016/09/14/batch_normalization/
-//
-//    size_t inSz = insz.w * insz.h * insz.d, bsz = insz.n;
-//
-//    /// ∂f/∂β = ∑∂f/∂y
-//    cblas_sgemv(CBLAS_ORDER::CblasRowMajor,
-//        CBLAS_TRANSPOSE::CblasTrans,
-//        blasint(bsz),
-//        blasint(inSz),
-//        1.F,
-//        gradIn,
-//        blasint(inSz),
-//        prm.onc,
-//        blasint(1),
-//        0.F,
-//        prm.dSchift,
-//        blasint(1));
-//
-//    /// ∂f/∂γ = ∑∂f/∂y ⋅ ^x
-//    for (size_t i = 0; i < inSz; ++i){
-//
-//        snFloat* igr = gradIn + i, *norm = prm.norm + i, dScale = 0.F;
-//        for (size_t j = 0; j < bsz; ++j){
-//
-//            dScale += igr[0] * norm[0];
-//
-//            norm += inSz;
-//            igr += inSz;
-//        }
-//        prm.dScale[i] = dScale;
-//    }
-//
-//    /// ∂f/∂x = (m⋅γ⋅∂f/∂y − γ⋅∂f/∂β − ^x⋅γ⋅∂f/∂γ) / m⋅σ2
-//    for (size_t j = 0; j < bsz; ++j){
-//
-//        snFloat* igr = gradIn + j * inSz, *ogr = gradOut + j * inSz, *norm = prm.norm + j * inSz;
-//        for (size_t i = 0; i < inSz; ++i)
-//            ogr[i] = prm.scale[i] * (igr[i] * bsz - prm.dSchift[i] - norm[i] * prm.dScale[i]) / (prm.varce[i] * bsz);
-//    }
-//    for (size_t i = 0; i < inSz; ++i){
-//        prm.schift[i] -= prm.dSchift[i] * prm.lr;
-//        prm.scale[i] -= prm.dScale[i] * prm.lr;
-//    }
-//
-//}
+__global__ void calcMeanAndVarce(SN_Base::snSize insz, snFloat* in, batchNorm prm){
+
+    size_t sz = insz.w * insz.h * insz.d,
+           bsz = insz.n;
+
+    // gridDim.x = insz.d
+    // blockDim.x <= insz.w * insz.h
+
+    in += insz.w * insz.h * blockIdx.x;
+    prm.mean += insz.w * insz.h * blockIdx.x;
+    prm.varce += insz.w * insz.h * blockIdx.x;
+
+    unsigned int i = threadIdx.x;
+    while (i < (insz.w * insz.h)){
+
+        snFloat srq = 0.F;
+        for (size_t j = 0; j < bsz; ++j){
+           
+            snFloat cin = in[i + j * sz];
+
+            prm.mean[i] += cin;
+
+            srq += cin * cin;
+        }
+               
+        prm.mean[i] /= bsz;
+        srq /= bsz;
+
+        prm.varce[i] = sqrt(srq - prm.mean[i] * prm.mean[i] + 0.00001F);
+                       
+        i += blockDim.x;
+    }
+}
+
+__global__ void calcNormAndOut(SN_Base::snSize insz, snFloat* in, snFloat* out, batchNorm prm){
+       
+    size_t sz = insz.w * insz.h * insz.d;
+
+    // gridDim.x = insz.d
+    // gridDim.y = insz.n
+    // blockDim.x <= insz.w * insz.h
+    
+    prm.mean += blockIdx.x * insz.w * insz.h;
+    prm.varce += blockIdx.x * insz.w * insz.h;
+    prm.scale += blockIdx.x * insz.w * insz.h;
+    prm.schift += blockIdx.x * insz.w * insz.h;
+
+    snFloat* cin = in + blockIdx.x * insz.w * insz.h + blockIdx.y * sz,
+           * cout = out + blockIdx.x * insz.w * insz.h + blockIdx.y * sz,
+           * norm = prm.norm + blockIdx.x * insz.w * insz.h + blockIdx.y * sz;
+
+    unsigned int i = threadIdx.x;
+    while (i < (insz.w * insz.h)){
+               
+        norm[i] = (cin[i] - prm.mean[i]) / prm.varce[i];
+        cout[i] = norm[i] * prm.scale[i] + prm.schift[i];
+       
+        i += blockDim.x;
+    }    
+}
+
+__global__ void calcDSchiftAndDScale(SN_Base::snSize insz, snFloat* gradIn, batchNorm prm){
+
+    size_t sz = insz.w * insz.h * insz.d,
+           bsz = insz.n;
+
+    // gridDim.x <= insz.d
+    // blockDim.x <= inSz
+
+    gradIn += insz.w * insz.h * blockIdx.x;
+    prm.norm += insz.w * insz.h * blockIdx.x;
+    prm.dSchift += insz.w * insz.h * blockIdx.x;
+    prm.dScale += insz.w * insz.h * blockIdx.x;
+
+    unsigned int i = threadIdx.x;
+    while (i < (insz.w * insz.h)){
+
+        snFloat dScale = 0.F;
+        for (size_t j = 0; j < bsz; ++j){
+            
+            snFloat cin = gradIn[i + j * sz],
+                    norm = prm.norm[i + j * sz];
+          
+            prm.dSchift[i] += cin;
+            
+            dScale += cin * norm;
+        }
+        prm.dScale[i] = dScale;
+              
+        i += blockDim.x;
+    }
+}
+
+__global__ void calcGrOut(SN_Base::snSize insz, snFloat* gradIn, snFloat* gradOut, batchNorm prm){
+
+    size_t sz = insz.w * insz.h * insz.d,
+           bsz = insz.n;
+
+    // gridDim.x = insz.d
+    // gridDim.x = insz.n
+    // blockDim.x <= insz.w * insz.h
+
+    prm.scale += insz.w * insz.h * blockIdx.x;
+    prm.varce += insz.w * insz.h * blockIdx.x;
+    prm.dSchift += insz.w * insz.h * blockIdx.x;
+    prm.dScale += insz.w * insz.h * blockIdx.x;
+
+    snFloat* igr = gradIn + blockIdx.x * insz.w * insz.h + blockIdx.y * sz,
+           * ogr = gradOut + blockIdx.x * insz.w * insz.h + blockIdx.y * sz,
+           * norm = prm.norm + blockIdx.x * insz.w * insz.h + blockIdx.y * sz;
+       
+    unsigned int i = threadIdx.x;
+    while (i < (insz.w * insz.h)){
+
+        ogr[i] = prm.scale[i] * (igr[i] * bsz - prm.dSchift[i] - norm[i] * prm.dScale[i]) / (prm.varce[i] * bsz);
+
+        i += blockDim.x;
+    }
+}
+
+__global__ void calcSchiftAndScale(SN_Base::snSize insz, batchNorm prm){
+       
+    // gridDim.x = insz.d
+    // blockDim.x <= insz.w * insz.h
+
+    prm.schift += insz.w * insz.h * blockIdx.x;
+    prm.scale += insz.w * insz.h * blockIdx.x;
+    prm.dSchift += insz.w * insz.h * blockIdx.x;
+    prm.dScale += insz.w * insz.h * blockIdx.x;
+
+    unsigned int i = threadIdx.x;
+    while (i < (insz.w * insz.h)){
+              
+        prm.schift[i] -= prm.dSchift[i] * prm.lr;
+        prm.scale[i] -= prm.dScale[i] * prm.lr;
+
+        i += blockDim.x;
+    }
+}
+
+
+void batchNormForward(SN_Base::snSize insz, snFloat* in, snFloat* out, const batchNorm& prm){
+        
+    calcMeanAndVarce << < insz.d, 256 >> > (insz, in, prm);
+
+    dim3 dimBlock(128);
+    dim3 dimGrid(int(insz.d), int(insz.n));
+
+    calcNormAndOut << < dimGrid, dimBlock >> > (insz, in, out, prm);
+}
+
+void batchNormBackward(SN_Base::snSize insz, snFloat* gradIn, snFloat* gradOut, const batchNorm& prm){
+    // https://kevinzakka.github.io/2016/09/14/batch_normalization/
+    
+
+    calcDSchiftAndDScale << <insz.d, 256 >> > (insz, gradIn, prm);
+   
+    dim3 dimBlock(128);
+    dim3 dimGrid(int(insz.d), int(insz.n));
+
+    calcGrOut << < dimGrid, dimBlock >> > (insz, gradIn, gradOut, prm);
+
+    calcSchiftAndScale << <insz.d, 256 >> > (insz, prm);
+}
 
 
 void channelBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, snFloat* out, batchNorm prm){
@@ -157,13 +233,15 @@ void channelBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, s
 
         channelBatchNormInf << <dimGrid, dimBlock >> > (insz, in, out, prm);
     }
-   /* else{
+    else{
 
         size_t stepD = insz.w * insz.h,
                stepN = stepD * insz.d,
                bsz = insz.n;
 
-        snFloat* share = (snFloat*)calloc(stepD * bsz, sizeof(snFloat));
+        snFloat* share = nullptr;
+        cuAssert(cudaMalloc(&share, stepD * bsz * sizeof(snFloat)));
+       
         snSize sz(insz.w, insz.h, 1, insz.n);
 
         for (size_t i = 0; i < insz.d; ++i){
@@ -172,7 +250,7 @@ void channelBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, s
             snFloat* pIn = in + stepD * i;
             for (size_t j = 0; j < bsz; ++j){
 
-                memcpy(pSh, pIn, stepD * sizeof(snFloat));
+                cuAssert(cudaMemcpy(pSh, pIn, stepD * sizeof(snFloat), cudaMemcpyKind::cudaMemcpyDeviceToDevice));
                 pSh += stepD;
                 pIn += stepN;
             }
@@ -185,7 +263,7 @@ void channelBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, s
             pSh = share;
             snFloat* pOut = out + stepD * i;
             for (size_t j = 0; j < bsz; ++j){
-                memcpy(pOut, pSh, stepD * sizeof(snFloat));
+                cuAssert(cudaMemcpy(pOut, pSh, stepD * sizeof(snFloat), cudaMemcpyKind::cudaMemcpyDeviceToDevice));
                 pSh += stepD;
                 pOut += stepN;
             }
@@ -193,8 +271,8 @@ void channelBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, s
             prm.offset(stepD);
             prm.norm += stepD * bsz;
         }
-        free(share);
-    }*/
+        cuAssert(cudaFree(share));
+    }
 }
 
 void layerBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, snFloat* out, const batchNorm& prm){
@@ -206,11 +284,11 @@ void layerBatchNorm(bool fwBw, bool isLern, const snSize& insz, snFloat* in, snF
 
         layerBatchNormInf << <dimGrid, dimBlock >> > (insz, in, out, prm);
     }
-    //else{ // isLerning
-    //    if (fwBw)
-    //        batchNormForward(insz, in, out, prm);
-    //    else
-    //        batchNormBackward(insz, in, out, prm);
-    //}
+    else{ // isLerning
+        if (fwBw)
+            batchNormForward(insz, in, out, prm);
+        else
+            batchNormBackward(insz, in, out, prm);
+    }
 }
 
